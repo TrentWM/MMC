835bd205f4c0f302a40dc4be5ae51a6f5eba7c65
TrentWM <TrentWM@users.noreply.github.com>
2016-12-15 20:25:32 -0500

    Minor adjustments + added comments

--- mmc.py
+++ mmc.py
@@ -14,7 +14,7 @@ class MMC:
             self.arrival_time=arrival_time
             self.service_start = None
             self.service_time=service_time
-            self.service_end= None #self.service_start + self.service_time
+            self.service_end= None
             self.time_in_syst = None
             self.wait = wait
                 
@@ -29,7 +29,6 @@ class MMC:
         self.q = [] #queue/waiting line
         self.current_wait = 0
             
-        self.full = False #True if all servers in use -- prevents need to search customer list each time
         self.num_inservice = 0
         self.num_queued = 0
         self.next_departure = float('inf')
@@ -42,10 +41,8 @@ class MMC:
     def add_customer(self,arrival_time):
         '''adds another customer to this MMC node, at a server if available
         otherwise in the queue'''
-        #print('adding customer')
         
         if self.num_inservice < self.num_servers:
-    
             #server(s) available...add customer here
         
             s = self.choose_server() #can use this approach later for server-specific service rates
@@ -62,6 +59,7 @@ class MMC:
 
         else:
             #servers full, increment queue
+            
             service_time = r.expovariate(self.mu)
             c = self.Customer(arrival_time,service_time,self.current_wait)
             c.wait = self.current_wait
@@ -74,13 +72,17 @@ class MMC:
         self.q_log.append([len(self.q),arrival_time])
         self.arrival_log.append(arrival_time)
         
+        #return reference to the customer
         return c
               
     def choose_server(self):
-        '''selects an empty server for a new customer'''        
+        '''selects an empty server for a new customer'''  
+        
         #***need to add response here in case servers are full and no choice is made***        
         choice = None
         if self.num_inservice < self.num_servers:
+            #if at least one servers is available, select server by number to use
+            
             choice = 0 
             i = 0
             stop = False
@@ -91,35 +93,42 @@ class MMC:
                     stop = True
                 else:
                     i += 1
-         
+                    
+        #return integer number of chosen server
         return choice
             
     def find_next_departure(self):
-        '''checks for proximate departure from servers'''
+        '''checks for time of next departure from servers'''
         
         dep_time = float('inf')
         dep_c = None
         
         for c in self.service:
+            #iterate over servers...
+            
             if c != None:
-                #check nonempty servers for departure times
+                #...if in-use, check departure time
                 if c.service_end < dep_time:
+                    #keep record of nearest departure time
                     dep_time = c.service_end
                     dep_c = c
-                
+        
+        #return numerical time of departure and reference to customer departing         
         return dep_time, dep_c
         
     def update_departures(self,next_arrival):
-        '''will remove any customers from server who finish before next arrival'''        
+        '''remove any customers from server who finish service before next arrival time'''        
 
         num_departing = 0
         dep_time = 0
         
         while dep_time < next_arrival:
-            #keep checking servers/queue until next departure after given time
+            #continually check customers in system (queue and service)...
+            
             dep_time,c = self.find_next_departure()
             if dep_time<next_arrival:
-                #next departure leaving before next arrival,adjust queues and servers
+                #...if their departure time is before the next arrival, update their position in system accordingly
+                
                 if self.q:
                     #move customer from queue into service spot of departing customer
                     #record service endtime and time in system
